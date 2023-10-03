da525566e11a0f04521eb85631a5992959b5314b
RitchilHima <himaritchil123@gmailcom>
2023-10-04 02:06:12 +0600

    first commit

---
+++ Js/script.js
@@ -0,0 +1,23 @@
+const birthday = "2 January 2024";
+
+function countdown(){
+    const birthdayDate = new Date(birthday);
+    const currentDate = new Date();
+
+    const totalSeconds = (birthdayDate - currentDate)/1000;
+
+    const days = Math.floor(totalSeconds / (3600*24));
+    const hours = Math.floor(totalSeconds % (3600*24)/(3600));
+    const minutes = Math.floor(totalSeconds % 3600/60);
+    const seconds = Math.floor(totalSeconds % 60);
+    document.getElementById("days").innerText = formatTime (days);
+    document.getElementById("minutes").innerText = formatTime (minutes);
+    document.getElementById("hours").innerText = formatTime (hours);
+    document.getElementById("seconds").innerText = formatTime (seconds);
+}
+
+function formatTime(time){
+    return time < 10? (`0${time}`) : time;
+}
+setInterval(countdown, 1000);
+countdown();
